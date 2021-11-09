5e7bf507f6b490cb1b08c60aea951ac8cefa102d
s-potato <sssssasas09@gmail.com>
2021-11-09 16:49:41 +0700

    add register route

--- chatserver/routes/users.js
+++ chatserver/routes/users.js
@@ -7,8 +7,18 @@ router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
 
-router.post('/', function (req, res, next) {
-    res.send(UserModel.createUser(req.body));
+router.post('/register', function (req, res, next) {
+    UserModel.create(req.body, function (err, result) {
+        if (err) //if error, return status: error, data: err
+            res.status(500).json({ status: "error", data: err });
+        else {  //if success, return username and email
+            res.json({ status: "success", message: "User added successfully!!!", data: (({ username, email }) => ({ username, email }))(result) });
+        }
+    });
+})
+
+router.post('/login', function(req, res, next) {
+    
 })
 
 module.exports = router;
